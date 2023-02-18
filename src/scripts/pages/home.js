0a6417a404154b6dace32c70a4ba152ebc92dc4b
stFrenky <shoma.albakov69@gmail.com>
2023-02-18 14:11:30 +0300

    fix style business

--- src/scripts/pages/home.js
+++ src/scripts/pages/home.js
@@ -1,1 +1,17 @@
 import '/src/styles/pages/home.scss';
+
+const businessItem = document.querySelector('.business-lines__item');
+const businessTextBox = document.querySelector('.business-lines__text-box');
+const businessText = document.querySelector('.business-lines__text');
+const businessArrow = document.querySelector('.business-lines__arrow');
+
+function toggleClass(btn, item) {
+	btn.addEventListener('click', () => {
+		item.classList.toggle('active');
+	})
+}
+
+toggleClass(businessArrow, businessArrow)
+toggleClass(businessArrow, businessTextBox)
+toggleClass(businessArrow, businessText)
+toggleClass(businessArrow, businessItem)
